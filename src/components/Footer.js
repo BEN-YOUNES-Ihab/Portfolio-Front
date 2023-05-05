40f0810932992792d764bc84272f3d4d53a8f966
ihab <ihab.ben.younes@hotmail.com>
2023-05-05 23:47:06 +0200

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ import {
           py={4}
           align="center"
         >
-          <Text>© 2023 BEN YOUNES Iha</Text>
+          <Text>© 2023 BEN YOUNES Ihab</Text>
         </Container>
       </Box>
     );
